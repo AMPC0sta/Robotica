ee13082662e298f442f70235aec55d519db38a18
AMPC0sta <75615924+AMPC0sta@users.noreply.github.com>
2024-12-14 15:27:13 +0000

    adding exec 1 and 2 resolution

---
+++ ExerciciosCoordenadas/FolhaExerciciosPosicaoOrientacaoEixosCoordenados.m
@@ -0,0 +1,49 @@
+% Exercicio 1) 
+% Resolvido em EixosCoordenadosExercicio1Pag18.m
+
+% Exercicio 2.a)
+% Resolvido em EixosCoordenadosExercicio2Pag19.m
+
+% Exercicio 2.b)
+% Resolvido em EixosCoordenadosExercicio2Pag19.m
+
+% Exercicio 2.c)
+% Resolvido em EixosCoordenadosExercicio2Pag19.m
+
+% Exercicio 2.d
+
+% Ponto P2)
+Ap2 = [2 4 0]';
+
+% Copiando os artefactos interessantes de EixosCoordenadosExercicio2Pag19.m
+
+% First  Operation: Rotation of 30 degrees in Z
+% Second Operation: Translation along the vector (4,3,0)
+theta = deg2rad(30);  % Rotation in Z
+trans_X = 4;
+trans_Y = 3;
+trans_Z = 0;
+
+% anonymous functions, can be called inside this very script
+% for rotation
+RotZ = @(x) [ 
+                [1 0 0 0];
+                [0 cos(x) -sin(x) 0];
+                [0 sin(x)  cos(x) 0]; 
+                [0 0 0 1] 
+            ];
+
+% for translation
+Trans = @(x,y,z) [
+                    [1 0 0 x];
+                    [0 1 0 y];
+                    [0 0 1 z];
+                    [0 0 0 1];
+                ];
+
+AtB = Trans(trans_X,trans_Y,trans_Z) * RotZ(theta);
+
+Bp2 = AtB * [Ap2;1];
+
+disp("Exercice 2d: Point P2 in referential B");
+disp(Bp2);
