9dcd36c69e330cb56d74e2c44d89a6cdddefce9d
ANTONIO.A.COSTA <antonio.a.costa@oracle.com>
2024-12-27 15:44:19 +0000

    removing matrices displaying for debuging on obstacle_avoidance function

--- tp2/obstacle_avoidance.m
+++ tp2/obstacle_avoidance.m
@@ -2,11 +2,9 @@ function [f_obstacle] = obstacle_avoidance(delta_theta,theta_obs,beta_1,beta_2,d
 
     lambda_obs_i = beta_1 * exp(-dist/beta_2);
 
-    disp(dist);
+    
     sigma_i = atan(tan(delta_theta/2)+((rob_W/2)./((rob_L/2)+dist)));
-    disp(sigma_i);
-
-
+    
     partial = 0;
     for i = 1:11
         f_component_i = lambda_obs_i(i) * -theta_obs(i) * exp((-theta_obs(i)^2)/(2*(sigma_i(i))^2));
