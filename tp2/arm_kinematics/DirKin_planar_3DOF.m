50bd2900ed13ce58bce58ae4aa5eda11d35a046f
ANTONIO.A.COSTA <antonio.a.costa@oracle.com>
2025-01-08 15:58:22 +0000

    adding comments to kinematics

--- tp2/arm_kinematics/DirKin_planar_3DOF.m
+++ tp2/arm_kinematics/DirKin_planar_3DOF.m
@@ -1,11 +1,11 @@
-function [error, pe]=DirKin_planar_3DOF(qinput,L,qmin,qmax)    % this function implement the direct kinematics of planar robot with 3DOF
-
-                                              % inputs:
-                                              % qinput=[theta1 theta2 theta3] - vetor with the joint values (rad)
-                                              % L=[L1 L2 L3] - vector with the two links lenghts (mm)
-                                         
-                                              % output:
-                                              % pe=[xe ye] - vetor with desired values for the end-effector position
+function [error, pe]=DirKin_planar_3DOF(qinput,L,qmin,qmax)    
+% this function implement the direct kinematics of planar robot with 3DOF
+% inputs:
+%           qinput=[theta1 theta2 theta3] - vetor with the joint values (rad)
+%           L=[L1 L2 L3] - vector with the two links lenghts (mm)
+%                                         
+% output:
+% pe=[xe ye] - vetor with desired values for the end-effector position
 
 error = 0;
 
@@ -16,7 +16,6 @@ for ptr = 1:length(qinput)
         error('Angle is outside joint limits!'); %#ok<NOEFF>
     end
 end
-
 
 q = [qinput(1),qinput(2),qinput(3)]; %Joint J1 is vertical, so that angle affects to Y axis instead of X, do this, we shift the quadrant
 xe=L(1)*cos(q(1))+L(2)*cos(q(1)+q(2))+L(3)*cos(q(1)+q(2)+q(3));
